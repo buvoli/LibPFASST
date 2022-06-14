0763356067a96434c229f2a8447c10df48975e9a
Tommaso Buvoli <tommasosignup@gmail.com>
2022-06-14 03:54:14 +0000

    Updated local install script

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# -----------------------------------------------------------------------------
+# Local initial setup script, after cloning
+#   git clone https://github.com/buvoli/LibPFASST.git
+#   git fetch origin
+#   git checkout --track origin/exp-parareal
+# -----------------------------------------------------------------------------
+
 set -e
-git switch -c exp-parareal
 make fftw3
 make libnpy
 make USE_FFT=TRUE USE_FFTW=TRUE
