56637f069dfe536034884684c197a2cc699f14c6
Tommaso Buvoli <tommasosignup@gmail.com>
2022-06-14 06:16:42 +0000

    Updated init-cori.sh

--- init-cori.sh
+++ init-cori.sh
@@ -3,6 +3,7 @@
 # -----------------------------------------------------------------------------
 # Cori initial setup script, after cloning
 #   git clone https://github.com/buvoli/LibPFASST.git
+#   cd LibPFASST
 #   git fetch origin
 #   git checkout --track origin/exp-parareal
 # -----------------------------------------------------------------------------
@@ -10,8 +11,13 @@
 set -e
 
 module load openmpi
+
+mv Makefile.defaults.cori Makefile.defaults # disable sweepers
+mv Makefile.external.cori Makefile.external # compiles libnpy with ifort
+
 make fftw3
 make libnpy
 make USE_FFT=TRUE USE_FFTW=TRUE
+mv *.mod include/ # added since ifort does not support -J Flag
 
 set +e
