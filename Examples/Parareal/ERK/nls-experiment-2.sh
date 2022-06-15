c6d30cd8d0170ce5a98a28172836ff13092dfb0b
Tommaso Buvoli <tommasosignup@gmail.com>
2022-06-15 20:53:45 +0000

    Added scripts for NLS experiments 2 and 3

---
+++ Examples/Parareal/ERK/nls-experiment-2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# =============================================================================
+#  Runs the second NLS experiment from paper
+# =============================================================================
+
+RUNNER='mpirun'
+FLAGS_ERK='-n 1'
+FLAGS_PARA='-n 1'
+
+make # ensure that code is compiled
+
+set -x # (print commands are they run)
+
+# Serial ERK (Reference)
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=2 nsteps_rk=524288 rho=0 outdir='"nls-exp2/serial-ref-"'
+
+# Serial ERK (Fine)
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=2 nsteps_rk=65536 rho=0.0245436926061703 outdir='"nls-exp2/serial-fine-"'
+
+# Parareal ERK K=0,..,6
+for i in {0..6}
+do
+    $RUNNER $FLAGS_ERK main.1d.exe params/NLS-parareal.nml ic_type=2 niters="${i}" rho=0.0245436926061703 outdir="\"nls-exp2/parareal-k-${i}-\""
+done
---
+++ Examples/Parareal/ERK/nls-experiment-3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# =============================================================================
+#  Runs the third NLS experiment from paper
+# =============================================================================
+
+RUNNER='mpirun'
+FLAGS_ERK='-n 1'
+FLAGS_PARA='-n 1'
+
+make # ensure that code is compiled
+
+set -x # (print commands are they run)
+
+# Serial ERK (Reference)
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=3 nsteps_rk=524288 rho=0 outdir='"nls-exp3/serial-ref-"'
+
+# Serial ERK (Fine)
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=3 nsteps_rk=65536 rho=0.0245436926061703 outdir='"nls-exp3/serial-fine-"'
+
+# Parareal ERK NG=1,...,3 and K=0,...,6
+for j in {1..3}
+do
+    for i in {0..6}
+    do
+        $RUNNER $FLAGS_PARA main.1d.exe params/NLS-parareal.nml ic_type=3 niters="${i}" nsteps_rk="${j} 32" rho=0.0245436926061703 outdir="\"nls-exp3/parareal-ng-${j}-k-${i}-\""
+    done
+done
