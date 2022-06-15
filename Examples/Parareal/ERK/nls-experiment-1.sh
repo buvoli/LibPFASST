abd4f135453bc4ca79f4f42ff061b35cf68b9415
Tommaso Buvoli <tommasosignup@gmail.com>
2022-06-15 20:53:33 +0000

    Updated NLS experiment 1 script

--- Examples/Parareal/ERK/nls-experiment-1.sh
+++ Examples/Parareal/ERK/nls-experiment-1.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-make USE_FFT=TRUE USE_FFTW=TRUE
+# =============================================================================
+#  Runs the first NLS experiment from paper
+# =============================================================================
 
-# # Serial ERK (Reference)
-./main.1d.exe params/NLS-serial-fine.nml ic_type=1 nsteps_rk=524288 rho=0 outdir='"nls-exp1/serial-ref"'
+RUNNER='mpirun'
+FLAGS_ERK='-n 1'
+FLAGS_PARA='-n 1'
+
+make # ensure that code is compiled
+
+set -x # (print commands are they run)
+
+# Serial ERK (Reference)
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=1 nsteps_rk=524288 rho=0 outdir='"nls-exp1/serial-ref-"'
 
 # Serial ERK (Fine)
-./main.1d.exe params/NLS-serial-fine.nml ic_type=1 nsteps_rk=65536 rho=0.0245436926061703 outdir='"nls-exp1/serial-fine"'
+$RUNNER $FLAGS_ERK main.1d.exe params/NLS-serial-fine.nml ic_type=1 nsteps_rk=65536 rho=0.0245436926061703 outdir='"nls-exp1/serial-fine-"'
 
 # Parareal ERK K=0,..,6
 for i in {0..6}
 do
-    mpirun -n 8 --oversubscribe ./main.1d.exe params/NLS-parareal.nml ic_type=1 niters="${i}" rho=0.024543692606170 outdir="\"nls-exp1/parareal-k-${i}\""
+    $RUNNER $FLAGS_ERK main.1d.exe params/NLS-parareal.nml ic_type=1 niters="${i}" rho=0.0245436926061703 outdir="\"nls-exp1/parareal-k-${i}-\""
 done
