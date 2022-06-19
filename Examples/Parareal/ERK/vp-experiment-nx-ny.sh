57063458c71d2e6b1f93fc73d3cc5d38e30e8bb9
Tommaso Buvoli <tommasosignup@gmail.com>
2022-06-19 01:29:26 +0000

    Added script vp-experiment-nx-ny.sh to run variable grid size

---
+++ Examples/Parareal/ERK/vp-experiment-nx-ny.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# =============================================================================
+#  Runs the VP experiment from paper with variable nx,ny for testing
+# =============================================================================
+
+# RUNNER='mpirun'
+# FLAGS_ERK='-n 1'
+# FLAGS_PARA='-n 4'
+
+RUNNER='srun'
+FLAGS_ERK='--nodes=1 --ntasks=1'
+FLAGS_PARA='--nodes=64 --ntasks=2048 --ntasks-per-node=32 --cpus-per-task=1'
+
+nx=128
+ny=128
+base_dir="vp-nx-${nx}-ny-${ny}"
+
+make DIM=2 # ensure that code is compiled
+
+set -x # (print commands are they run)
+
+# Serial ERK (Reference)
+if [ ! -d "dat/${base_dir}/serial-ref-P0001/" ] ; then
+    $RUNNER $FLAGS_ERK main.2d.exe params/VP-serial-fine.nml nsteps_rk=262144 nx="${nx}" ny="${ny}" rho=0 outdir="\"${base_dir}/serial-ref-\""
+fi
+
+# Serial ERK (Fine)
+if [ ! -d "dat/${base_dir}/serial-fine-P0001/" ] ; then
+    $RUNNER $FLAGS_ERK main.2d.exe params/VP-serial-fine.nml nsteps_rk=131072 nx="${nx}" ny="${ny}" rho=0.0245436926061703 outdir="\"${base_dir}/serial-fine-\""
+fi
+
+# Parareal ERK NG=1,...,3 and K=0,...,12
+for j in {1..3}
+do
+    for i in {0..12}
+    do
+        $RUNNER $FLAGS_PARA main.2d.exe params/VP-parareal.nml nx="${nx} ${nx}" ny="${ny} ${ny}" niters="${i}" nsteps_rk="${j} 64" rho=0.0245436926061703 outdir="\"${base_dir}/parareal-ng-${j}-k-${i}-\""
+    done
+done
